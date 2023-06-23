9cdb12211c72399c9cd54cd48ff75d11a3e4e7cc
DoneshD <donesh.dava@gmail.com>
2023-06-23 07:09:42 +0000

    Working on movement

--- FromTheAshesReborn/Private/PlayerCharacter.h
+++ FromTheAshesReborn/Private/PlayerCharacter.h
@@ -15,15 +15,26 @@ public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+
+	void LookUp(float AxisValue);
+	void LookRight(float AxisValue);
+
+
+
 };
+
+
--- FromTheAshesReborn/Public/PlayerCharacter.cpp
+++ FromTheAshesReborn/Public/PlayerCharacter.cpp
@@ -29,6 +29,33 @@ void APlayerCharacter::Tick(float DeltaTime)
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("MoveFoward"), this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &APlayerCharacter::MoveRight);
+
+	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &APlayerCharacter::LookRight);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APlayerCharacter::LookUp);
 
 }
 
+void APlayerCharacter::MoveForward(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector() * AxisValue);
+	UE_LOG(LogTemp, Warning, TEXT("Test"));
+}
+
+void APlayerCharacter::MoveRight(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector() * AxisValue);
+	UE_LOG(LogTemp, Warning, TEXT("Test"));
+
+}
+
+void APlayerCharacter::LookUp(float AxisValue)
+{
+	AddControllerPitchInput(AxisValue);
+}
+
+void APlayerCharacter::LookRight(float AxisValue)
+{
+	AddControllerYawInput(AxisValue);
+}
