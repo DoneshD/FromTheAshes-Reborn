609f65e40bf74a37c6dd665af3c8a164d3d5fd2f
DoneshD <donesh.dava@gmail.com>
2023-07-04 05:33:36 +0000

    Refactored movement

---
+++ FromTheAshesReborn/Public/PlayerCharacter.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class FROMTHEASHESREBORN_API APlayerCharacter : public ACharacter
+{
+GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+public:
+	APlayerCharacter();
+
+
+
+protected:
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	void LookUp(float AxisValue);
+	void LookRight(float AxisValue);
+
+
+protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+};
