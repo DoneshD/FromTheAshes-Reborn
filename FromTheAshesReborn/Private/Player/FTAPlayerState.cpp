3bd76541003d4692b4bfc7a71cdd8511f66d5d43
DoneshD <donesh.dava@gmail.com>
2024-07-31 00:32:01 -0500

    Set up FTAPlayerState

--- FromTheAshesReborn/Private/Player/FTAPlayerState.cpp
+++ FromTheAshesReborn/Private/Player/FTAPlayerState.cpp
@@ -1,18 +1,33 @@
-﻿#include "Player/FTAPlayerState.h"
-#include "FTAAbilitySystem/FTAAbilitySystemComponent.h"
+﻿// Copyright 2020 Dan Kestranek.
+
+
+#include "Player/FTAPlayerState.h"
 #include "FTAAbilitySystem/FTAAttributeSet.h"
+#include "FTAAbilitySystem/FTAAbilitySystemComponent.h"
+#include "FTAAbilitySystem/FTAAbilitySystemGlobals.h"
+#include "Player/PlayerCharacter.h"
 
 AFTAPlayerState::AFTAPlayerState()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	// Create ability system component, and set it to be explicitly replicated
+	AbilitySystemComponent = CreateDefaultSubobject<UFTAAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
 
+	// Mixed mode means we only are replicated the GEs to ourself, not the GEs to simulated proxies. If another GDPlayerState (Hero) receives a GE,
+	// we won't be told about it by the Server. Attributes, GameplayTags, and GameplayCues will still replicate to us.
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	// Create the attribute set, this replicates by default
+	// Adding it as a subobject of the owning actor of an AbilitySystemComponent
+	// automatically registers the AttributeSet with the AbilitySystemComponent
+	AttributeSet = CreateDefaultSubobject<UFTAAttributeSet>(TEXT("AttributeSet"));
+
+	// Set PlayerState's NetUpdateFrequency to the same as the Character.
+	// Default is very low for PlayerStates and introduces perceived lag in the ability system.
+	// 100 is probably way too high for a shipping game, you can adjust to fit your needs.
 	NetUpdateFrequency = 100.0f;
 
-	AbilitySystemComponent = CreateDefaultSubobject<UFTAAbilitySystemComponent>("AbilitySystemComponent");
-	AbilitySystemComponent->SetIsReplicated(true);
-	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Full);
-	
-	AttributeSet = CreateDefaultSubobject<UFTAAttributeSet>("AttributeSet");
+	DeadTag = FGameplayTag::RequestGameplayTag("State.Dead");
 }
 
 UAbilitySystemComponent* AFTAPlayerState::GetAbilitySystemComponent() const
@@ -23,4 +38,49 @@ UAbilitySystemComponent* AFTAPlayerState::GetAbilitySystemComponent() const
 UFTAAttributeSet* AFTAPlayerState::GetAttributeSet() const
 {
 	return AttributeSet;
+}
+
+
+bool AFTAPlayerState::IsAlive() const
+{
+	return GetCurrentHealth() > 0.0f;
+}
+
+float AFTAPlayerState::GetCurrentHealth() const
+{
+	return AttributeSet->GetCurrentHealth();
+}
+
+float AFTAPlayerState::GetMaxHealth() const
+{
+	return AttributeSet->GetMaxHealth();
+}
+
+float AFTAPlayerState::GetMoveSpeed() const
+{
+	return AttributeSet->GetMoveSpeed();
+}
+
+void AFTAPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AbilitySystemComponent)
+	{
+		// Attribute change callbacks
+		HealthChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSet->GetCurrentHealthAttribute()).AddUObject(this, &AFTAPlayerState::HealthChanged);
+	}
+}
+
+void AFTAPlayerState::HealthChanged(const FOnAttributeChangeData& Data)
+{
+	// Check for and handle knockdown and death
+	APlayerCharacter* PC = Cast<APlayerCharacter>(GetPawn());
+	if (IsValid(PC) && !IsAlive() && !AbilitySystemComponent->HasMatchingGameplayTag(DeadTag))
+	{
+		if (PC)
+		{
+			PC->FinishDying();
+		}
+	}
 }
--- FromTheAshesReborn/Public/Player/FTAPlayerState.h
+++ FromTheAshesReborn/Public/Player/FTAPlayerState.h
@@ -3,27 +3,59 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
 #include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
 #include "FTAPlayerState.generated.h"
 
 class UFTAAbilitySystemComponent;
 class UFTAAttributeSet;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FFTAOnGameplayAttributeValueChangedDelegate, FGameplayAttribute, Attribute, float, NewValue, float, OldValue);
+
 UCLASS()
 class FROMTHEASHESREBORN_API AFTAPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
-
-protected:
-	TObjectPtr<UFTAAbilitySystemComponent> AbilitySystemComponent;
-
-	UPROPERTY()
-	TObjectPtr<UFTAAttributeSet> AttributeSet;
-
+	
 public:
 	AFTAPlayerState();
-	
-	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
-	virtual UFTAAttributeSet* GetAttributeSet() const;
+	// Implement IAbilitySystemInterface
+	class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	class UFTAAttributeSet* GetAttributeSet() const;
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState")
+	bool IsAlive() const;
+	
+	/**
+	* Getters for attributes from GDAttributeSetBase. Returns Current Value unless otherwise specified.
+	*/
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState | Attributes")
+	float GetCurrentHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState | Attributes")
+	float GetMaxHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "PlayerState | Attributes")
+	float GetMoveSpeed() const;
+
+protected:
+	FGameplayTag DeadTag;
+
+	UPROPERTY()
+	class UFTAAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY()
+	class UFTAAttributeSet* AttributeSet;
+
+	// Attribute changed delegate handles
+	FDelegateHandle HealthChangedDelegateHandle;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Attribute changed callbacks
+	virtual void HealthChanged(const FOnAttributeChangeData& Data);
 
 };
