31b639436b54bfe2ce782edaa43e777e6d92ee58
DoneshD <donesh.dava@gmail.com>
2024-10-27 00:31:28 -0500

    Vault trace WIP

--- FromTheAshesReborn/Private/Movement/GroundedParkourComponent.cpp
+++ FromTheAshesReborn/Private/Movement/GroundedParkourComponent.cpp
@@ -3,6 +3,7 @@
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 
 UGroundedParkourComponent::UGroundedParkourComponent()
@@ -66,22 +67,133 @@ bool UGroundedParkourComponent::ParkourMasterTracerVault()
 		UE_LOG(LogTemp, Warning, TEXT("Character is falling"));
 		return false;
 	}
-	FHitResult HitResult;
-	GetForwardDistanceTrace(HitResult);
+	GetForwardActorTrace();
 	return false;
 }
 
 
-bool UGroundedParkourComponent::GetForwardDistanceTrace(FHitResult& OutHitResult)
+bool UGroundedParkourComponent::GetForwardActorTrace()
 {
-	FVector StartLocation = OwnerCharacter->GetActorLocation();
-	FVector EndLocation = (OwnerCharacter->GetActorForwardVector() * HorizontalTraceDistanceVault) + StartLocation;
+    FHitResult ForwardHitResult;
+    FVector StartLocation = OwnerCharacter->GetActorLocation();
+    FVector EndLocation = (OwnerCharacter->GetActorForwardVector() * TraceForwardActorDistance) + StartLocation;
+
+    FCollisionObjectQueryParams CollisionParams;
+    CollisionParams.AddObjectTypesToQuery(ECC_WorldStatic);
+
+    bool bHit = GetWorld()->LineTraceSingleByObjectType(ForwardHitResult, StartLocation, EndLocation, CollisionParams);
+
+    DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 1.0f, 0, 2.0f);
+
+    if (bHit)
+    {
+        DrawDebugPoint(GetWorld(), ForwardHitResult.ImpactPoint, 40.0f, FColor::Green, false, 1.0f);
+    }
+
+    TraceFirstActor = ForwardHitResult.GetActor();
+    TraceFirstActorImpactPoint = ForwardHitResult.ImpactPoint;
+
+
+    if (ForwardHitResult.bBlockingHit)
+    {
+        bool bHitVert = GetVerticalBlockerTrace();
+        return bHitVert;
+    }
+
+    CanParkour = false;
+    return false;
+
+}
+
+bool UGroundedParkourComponent::GetVerticalBlockerTrace()
+{
+	FHitResult VerticalHitResult;
+
+	if (!TraceFirstActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TraceFirstActor is NULL"));
+		return false;
+	}
+
+	FVector ActorOrigin, ActorBoxExtent;
+	TraceFirstActor->GetActorBounds(true, ActorOrigin, ActorBoxExtent);
+
+	FVector ActorTop = ActorOrigin + FVector(0, 0, ActorBoxExtent.Z);
+
+	FVector StartLocation = TraceFirstActorImpactPoint;
+	StartLocation.Z = ActorTop.Z;
+
+	FVector EndLocation = StartLocation + FVector(0, 0, CheckVerticalBlockerDistance); 
+
+	DrawDebugPoint(GetWorld(), StartLocation, 40.0f, FColor::Purple, false, 1.0f);
+	DrawDebugPoint(GetWorld(), EndLocation, 40.0f, FColor::Turquoise, false, 1.0f);
 
 	FCollisionObjectQueryParams CollisionParams;
 	CollisionParams.AddObjectTypesToQuery(ECC_WorldStatic);
 
-	GetWorld()->LineTraceSingleByObjectType(OutHitResult, StartLocation, EndLocation, CollisionParams);
-	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 2.0f);
-	return false;
+	bool bHit = GetWorld()->LineTraceSingleByObjectType(VerticalHitResult, StartLocation, EndLocation, CollisionParams);
+
+	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 1.0f, 0, 2.0f);
+
+	if (bHit)
+	{
+		DrawDebugPoint(GetWorld(), VerticalHitResult.ImpactPoint, 40.0f, FColor::Green, false, 1.0f);
+	}
+
+	//StartLocation.X = FMath::Max(StartLocation.X + 10.0f, ActorTop.Z); 
+
+	EndLocation = StartLocation + FVector(CheckVerticalBlockerDistance, 0, 0); 
+
+	//DrawDebugPoint(GetWorld(), StartLocation, 40.0f, FColor::Emerald, false, 1.0f);
+	DrawDebugPoint(GetWorld(), EndLocation, 40.0f, FColor::Orange, false, 1.0f);
+	
+
+	bool bHit2 = GetWorld()->LineTraceSingleByObjectType(VerticalHitResult, StartLocation, EndLocation, CollisionParams);
+
+	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 1.0f, 0, 2.0f);
+
+	if (bHit)
+	{
+		DrawDebugPoint(GetWorld(), VerticalHitResult.ImpactPoint, 40.0f, FColor::Green, false, 1.0f);
+	}
+
+	//StartLocation.Y = FMath::Max(StartLocation.Y + 10.0f, ActorTop.Z); 
+
+	EndLocation = StartLocation + FVector(0, CheckVerticalBlockerDistance, 0); 
+
+	//DrawDebugPoint(GetWorld(), StartLocation, 40.0f, FColor::Emerald, false, 1.0f);
+	DrawDebugPoint(GetWorld(), EndLocation, 40.0f, FColor::Orange, false, 1.0f);
+	
+
+	bool bHit3 = GetWorld()->LineTraceSingleByObjectType(VerticalHitResult, StartLocation, EndLocation, CollisionParams);
+
+	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 1.0f, 0, 2.0f);
+
+	if (bHit)
+	{
+		DrawDebugPoint(GetWorld(), VerticalHitResult.ImpactPoint, 40.0f, FColor::Green, false, 1.0f);
+	}
+
+
+	//StartLocation.Y = FMath::Max(StartLocation.Y + 10.0f, ActorTop.Z); 
+
+	EndLocation = StartLocation + FVector(0, -CheckVerticalBlockerDistance, 0); 
+
+	//DrawDebugPoint(GetWorld(), StartLocation, 40.0f, FColor::Emerald, false, 1.0f);
+	DrawDebugPoint(GetWorld(), EndLocation, 40.0f, FColor::Orange, false, 1.0f);
+	
+
+	bool bHit4 = GetWorld()->LineTraceSingleByObjectType(VerticalHitResult, StartLocation, EndLocation, CollisionParams);
+
+	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 1.0f, 0, 2.0f);
+
+	if (bHit)
+	{
+		DrawDebugPoint(GetWorld(), VerticalHitResult.ImpactPoint, 40.0f, FColor::Green, false, 1.0f);
+	}
+
+
+	return bHit;
 }
+
 
--- FromTheAshesReborn/Public/Movement/GroundedParkourComponent.h
+++ FromTheAshesReborn/Public/Movement/GroundedParkourComponent.h
@@ -30,20 +30,34 @@ protected:
 	UPROPERTY()
 	float JumpZVelocity;
 
+	UPROPERTY()
+	bool CanParkour;
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grounded Parkour System|Vault")
-	float HorizontalTraceDistanceVault = 220.0f;
+	float TraceForwardActorDistance = 300.0f;
+
+	UPROPERTY()
+	TObjectPtr<AActor> TraceFirstActor;
+
+	UPROPERTY()
+	FVector TraceFirstActorImpactPoint;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grounded Parkour System|Vault")
+	float CheckVerticalBlockerDistance = 150.0f;
 
 public:
 	UGroundedParkourComponent();
 	
 	virtual void BeginPlay() override;
 	
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-	                           FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	void ParkourInputPressedVault();
 
 	bool ParkourMasterTracerVault();
 
-	bool GetForwardDistanceTrace(FHitResult& OutHitResult);
+	bool GetForwardActorTrace();
+
+	bool GetVerticalBlockerTrace();
+
 };
