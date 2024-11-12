6fde34b39c9675799e0b321f95a9475674b72683
DoneshD <donesh.dava@gmail.com>
2024-11-12 00:51:00 -0600

    Adding data assets and tags

---
+++ FromTheAshesReborn/Private/DataAsset/FTACharacterData.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataAsset/FTACharacterData.h"
+
+UFTACharacterData::UFTACharacterData(const FObjectInitializer& ObjectInitializer)
+{
+}
---
+++ FromTheAshesReborn/Private/FTAAbilitySystem/TagRelationships/FTAAbilityTagRelationshipMapping.cpp
@@ -0,0 +1,4 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FTAAbilitySystem/TagRelationships/FTAAbilityTagRelationshipMapping.h"
