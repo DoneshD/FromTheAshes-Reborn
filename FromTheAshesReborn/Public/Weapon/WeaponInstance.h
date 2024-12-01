f8e2596941e8836706fe31f0c0319b163eca2407
DoneshD <donesh.dava@gmail.com>
2024-11-30 22:14:23 -0600

    Reworking equipment to weapon

---
+++ FromTheAshesReborn/Public/Weapon/WeaponInstance.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "WeaponInstance.generated.h"
+
+UCLASS()
+class FROMTHEASHESREBORN_API UWeaponInstance : public UObject
+{
+	GENERATED_BODY()
+};
