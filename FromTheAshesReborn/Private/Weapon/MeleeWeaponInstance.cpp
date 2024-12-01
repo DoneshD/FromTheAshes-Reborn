e9fbf4595c0bf238f5bfaac999e1194b67b72c18
DoneshD <donesh.dava@gmail.com>
2024-11-30 23:28:45 -0600

    Reworking equipment to weapon

--- FromTheAshesReborn/Private/Weapon/MeleeWeaponInstance.cpp
+++ FromTheAshesReborn/Private/Weapon/MeleeWeaponInstance.cpp
@@ -1,4 +1,1 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Weapon/MeleeWeaponInstance.h"
+﻿#include "Weapon/MeleeWeaponInstance.h"
--- FromTheAshesReborn/Public/Weapon/MeleeWeaponInstance.h
+++ FromTheAshesReborn/Public/Weapon/MeleeWeaponInstance.h
@@ -9,4 +9,12 @@ UCLASS()
 class FROMTHEASHESREBORN_API UMeleeWeaponInstance : public UWeaponInstance
 {
 	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Weapon Config")
+	float MaxDamage = 100.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Weapon Config")
+	FRuntimeFloatCurve DistanceDamageFalloff;
 };
