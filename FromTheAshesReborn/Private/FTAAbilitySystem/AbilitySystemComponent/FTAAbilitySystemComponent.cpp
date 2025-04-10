966ac16b12672db5cfaadbbce9e5fb0bd5d9a59d
DoneshD <donesh.dava@gmail.com>
2025-04-09 23:04:40 -0500

    Removed old queue input comment

--- FromTheAshesReborn/Private/FTAAbilitySystem/AbilitySystemComponent/FTAAbilitySystemComponent.cpp
+++ FromTheAshesReborn/Private/FTAAbilitySystem/AbilitySystemComponent/FTAAbilitySystemComponent.cpp
@@ -115,67 +115,12 @@ void UFTAAbilitySystemComponent::CancelInputActivatedAbilities()
 
 void UFTAAbilitySystemComponent::AbilityInputTagPressed(const FGameplayTag& InputTag)
 {
-
-	// FGameplayAbilitySpec InputedAbilitySpec;
-	// if (InputTag.IsValid())
-	// {
-	// 	for (const FGameplayAbilitySpec& AbilitySpec : ActivatableAbilities.Items)
-	// 	{
-	// 		if (AbilitySpec.Ability)
-	// 		{
-	// 			if (AbilitySpec.DynamicAbilityTags.HasTagExact(InputTag))
-	// 			{
-	// 				bool CanActivate = AbilitySpec.Ability->CanActivateAbility(AbilitySpec.Handle, AbilityActorInfo.Get());
-	// 				if(CanActivate)
-	// 				{
-	// 					InputedAbilitySpec = AbilitySpec;
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
-	//
-	// for (const FGameplayAbilitySpec& AbilitySpec : ActivatableAbilities.Items)
-	// {
-	// 	//If there is input during an active ability, check if it can be cancel current and activate
-	// 	
-	// 	// if(AbilitySpec.IsActive() && PCM->IsInInputQueueWindow == true)
-	// 	if(AbilitySpec.IsActive())
-	// 	{
-	// 		
-	// 		UFTAGameplayAbility* CurrentFTAAbility = Cast<UFTAGameplayAbility>(AbilitySpec.Ability);
-	// 		
-	// 		if(CurrentFTAAbility->CanBeCanceledForQueue)
-	// 		{
-	// 			UFTAGameplayAbility* InputedFTAAbility = Cast<UFTAGameplayAbility>(InputedAbilitySpec.Ability);
-	// 	
-	// 			if(InputedFTAAbility)
-	// 			{
-	// 				if(CurrentFTAAbility->QueueableAbilitiesTags.HasTag(InputedFTAAbility->UniqueIdentifierTag))
-	// 				{
-	// 					QueuedAbilitySpec = InputedAbilitySpec;
-	// 					return;
-	// 				}
-	// 			}
-	// 		}
-	// 		//Temporary solution for jumping, also fixes strange issue with launcher->air combos
-	// 		else
-	// 		{
-	// 			CancelAbility(CurrentFTAAbility);
-	// 		}
-	// 	}
-	// }
-	
-	
 	bool BlockingAbilityActive = ActivationGroupCount[(uint8)EFTAAbilityActivationGroup::Exclusive_Blocking] > 0;
 	bool ReplaceableAbilityActive = ActivationGroupCount[(uint8)EFTAAbilityActivationGroup::Exclusive_Replaceable] > 0;
 	if(BlockingAbilityActive)
 	{
 		if (InputTag.IsValid())
 		{
-			UE_LOG(LogTemp, Warning, TEXT("ASC InputTag: %s, "),
-			*InputTag.ToString());
-			UE_LOG(LogTemp, Warning, TEXT("InputTag.IsValid()"))
 			OnInputQueueReceived.Broadcast(InputTag);
 			return;
 		}
