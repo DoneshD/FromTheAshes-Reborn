079026b7c978a84aa6ab5ee5f6668ffa5605f880
DoneshD <donesh.dava@gmail.com>
2025-06-15 20:34:46 -0500

    Adding camera manager

---
+++ FromTheAshesReborn/Public/Player/FTAPlayerCameraManger.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "FTAPlayerCameraManger.generated.h"
+
+
+UCLASS()
+class FROMTHEASHESREBORN_API AFTAPlayerCameraManger : public APlayerCameraManager
+{
+	GENERATED_BODY()
+};
