5232453a6207e6088e0eee2d478197fa63112f60
DoneshD <donesh.dava@gmail.com>
2025-06-25 20:14:13 -0500

    State tree task set up

---
+++ FromTheAshesReborn/Private/StateTree/Tasks/STT_MeleeWeaponAttack.cpp
@@ -0,0 +1,31 @@
+﻿#include "StateTree/Tasks/STT_MeleeWeaponAttack.h"
+#include "GameFramework/Actor.h"
+#include "StateTreeExecutionContext.h"
+
+EStateTreeRunStatus FStateTreeTask_MeleeWeaponAttack::EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	AActor* OwnerActor = Cast<AActor>(Context.GetOwner());
+	const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	if (OwnerActor)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Event Tag: %s for Actor: %s"), *InstanceData.EventTag.ToString(), *OwnerActor->GetName());
+	}
+
+	return EStateTreeRunStatus::Succeeded;
+}
+
+EStateTreeRunStatus FStateTreeTask_MeleeWeaponAttack::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	return FStateTreeTaskCommonBase::Tick(Context, DeltaTime);
+}
+
+void FStateTreeTask_MeleeWeaponAttack::ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	FStateTreeTaskCommonBase::ExitState(Context, Transition);
+}
+
+EStateTreeRunStatus FStateTreeTask_MeleeWeaponAttack::ActivateMeleeAttack(const FStateTreeExecutionContext& Context) const
+{
+	return EStateTreeRunStatus::Running;
+}
---
+++ FromTheAshesReborn/Public/StateTree/Tasks/STT_MeleeWeaponAttack.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeTaskBase.h"
+#include "STT_MeleeWeaponAttack.generated.h"
+
+USTRUCT()
+struct FStateTreeTask_MeleeWeaponAttack_InstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Task")
+	FGameplayTag EventTag;
+};
+
+USTRUCT(DisplayName = "Melee Weapon Attack")
+struct FROMTHEASHESREBORN_API FStateTreeTask_MeleeWeaponAttack : public FStateTreeTaskCommonBase
+{
+	GENERATED_BODY()
+
+	using FInstanceDataType = FStateTreeTask_MeleeWeaponAttack_InstanceData;
+	virtual const UStruct* GetInstanceDataType() const override
+	{
+		return FInstanceDataType::StaticStruct();
+	}
+
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+	virtual void ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+
+private:
+	EStateTreeRunStatus ActivateMeleeAttack(const FStateTreeExecutionContext& Context) const;
+};
