ba769bc614680fde8a09843dc01325955f2dfebb
DoneshD <donesh.dava@gmail.com>
2025-08-25 17:59:21 -0500

    Start downed combat component

---
+++ FromTheAshesReborn/Private/CombatComponents/DownedCombatComponent.cpp
@@ -0,0 +1,19 @@
+﻿#include "DownedCombatComponent.h"
+
+UDownedCombatComponent::UDownedCombatComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+void UDownedCombatComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void UDownedCombatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
