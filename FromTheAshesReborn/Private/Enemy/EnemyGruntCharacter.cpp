3b27720d005cc7b63f6a3fef6a09c8dcae9b61ce
DoneshD <donesh.dava@gmail.com>
2025-09-29 14:04:38 -0500

    Creating grunt enemy

---
+++ FromTheAshesReborn/Private/Enemy/EnemyGruntCharacter.cpp
@@ -0,0 +1,25 @@
+﻿#include "Enemy/EnemyGruntCharacter.h"
+#include "FTACustomBase/FTACharacterMovementComponent.h"
+
+AEnemyGruntCharacter::AEnemyGruntCharacter(const class FObjectInitializer& ObjectInitializer) :
+	Super(ObjectInitializer.SetDefaultSubobjectClass<UFTACharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AEnemyGruntCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AEnemyGruntCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AEnemyGruntCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ FromTheAshesReborn/Public/Enemy/EnemyGruntCharacter.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBaseCharacter.h"
+#include "EnemyGruntCharacter.generated.h"
+
+UCLASS()
+class FROMTHEASHESREBORN_API AEnemyGruntCharacter : public AEnemyBaseCharacter
+{
+	GENERATED_BODY()
+
+protected:
+	
+	AEnemyGruntCharacter(const class FObjectInitializer& ObjectInitializer);
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
