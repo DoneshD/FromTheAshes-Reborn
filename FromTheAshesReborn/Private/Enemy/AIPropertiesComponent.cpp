95996a42784b5468189cfbf13ec7662f368d7d17
DoneshD <donesh.dava@gmail.com>
2025-09-30 17:12:33 -0500

    Adding AI Properties component

---
+++ FromTheAshesReborn/Private/Enemy/AIPropertiesComponent.cpp
@@ -0,0 +1,20 @@
+﻿#include "Enemy/AIPropertiesComponent.h"
+
+UAIPropertiesComponent::UAIPropertiesComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+void UAIPropertiesComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void UAIPropertiesComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
---
+++ FromTheAshesReborn/Public/Enemy/AIPropertiesComponent.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AIPropertiesComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FROMTHEASHESREBORN_API UAIPropertiesComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+protected:
+	
+	UAIPropertiesComponent();
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+};
