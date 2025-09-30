61be2ce037aae3e551417bddaa735a27f52cdb74
DoneshD <donesh.dava@gmail.com>
2025-09-30 17:14:20 -0500

    Subclassing Melee Attack for AI

---
+++ FromTheAshesReborn/Public/FTAAbilitySystem/GameplayAbilities/Attack/GA_AI_MeleeWeaponAttack.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_MeleeWeaponAttack.h"
+#include "GA_AI_MeleeWeaponAttack.generated.h"
+
+
+UCLASS()
+class FROMTHEASHESREBORN_API UGA_AI_MeleeWeaponAttack : public UGA_MeleeWeaponAttack
+{
+	GENERATED_BODY()
+};
