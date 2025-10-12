d37b297cabb7a24cb5856514e34f2a4fd87ad6e4
DoneshD <donesh.dava@gmail.com>
2025-10-11 19:28:22 -0500

    Move to working

--- FromTheAshesReborn/Private/StateTree/Tasks/STT_MoveToLocation.cpp
+++ FromTheAshesReborn/Private/StateTree/Tasks/STT_MoveToLocation.cpp
@@ -1,1 +1,30 @@
 ﻿#include "StateTree/Tasks/STT_MoveToLocation.h"
+
+#include "AIController.h"
+#include "StateTreeExecutionContext.h"
+#include "Kismet/GameplayStatics.h"
+#include "Navigation/PathFollowingComponent.h"
+
+EStateTreeRunStatus FStateTreeTask_MoveToLocation::EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	if(!InstanceData.InputActor)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Input Actor is null"))
+		return EStateTreeRunStatus::Failed;
+	}
+
+	EPathFollowingRequestResult::Type PathFollowingRequestResult = InstanceData.AIController->MoveToLocation(InstanceData.Location, InstanceData.AcceptableRadius);
+	
+	return EStateTreeRunStatus::Running;
+}
+
+EStateTreeRunStatus FStateTreeTask_MoveToLocation::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	return FStateTreeTaskCommonBase::Tick(Context, DeltaTime);
+}
+
+void FStateTreeTask_MoveToLocation::ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	FStateTreeTaskCommonBase::ExitState(Context, Transition);
+}
--- FromTheAshesReborn/Public/StateTree/Tasks/STT_MoveToLocation.h
+++ FromTheAshesReborn/Public/StateTree/Tasks/STT_MoveToLocation.h
@@ -4,15 +4,41 @@
 #include "StateTreeTaskBase.h"
 #include "STT_MoveToLocation.generated.h"
 
+class AAIController;
+
 USTRUCT()
+struct FROMTHEASHESREBORN_API FStateTreeTask_MoveToLocation_InstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	TObjectPtr<AActor> InputActor = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	TObjectPtr<AAIController> AIController = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	FVector Location;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	float AcceptableRadius = 300.0f;
+	
+};
+
+USTRUCT(DisplayName = "Move To Location")
 struct FROMTHEASHESREBORN_API FStateTreeTask_MoveToLocation : public FStateTreeTaskCommonBase
 {
 	GENERATED_BODY()
 
-private:
+	using FInstanceDataType = FStateTreeTask_MoveToLocation_InstanceData;
+	virtual const UStruct* GetInstanceDataType() const override
+	{
+		return FInstanceDataType::StaticStruct();
+	}
 
-protected:
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+	virtual void ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
 
-public:
 	
 };
