f16017ef81b9a5875b22466db0f54c90220a53a4
DoneshD <donesh.dava@gmail.com>
2025-10-23 09:34:26 -0500

    Rework move to task

---
+++ FromTheAshesReborn/Private/StateTree/Evaluators/STE_GetTargetLocation.cpp
@@ -0,0 +1,29 @@
+﻿#include "StateTree/Evaluators/STE_GetTargetLocation.h"
+
+#include "StateTreeExecutionContext.h"
+
+void FStateTreeEvaluator_GetTargetLocation::TreeStart(FStateTreeExecutionContext& Context) const
+{
+	FStateTreeEvaluatorCommonBase::TreeStart(Context);
+}
+
+void FStateTreeEvaluator_GetTargetLocation::TreeStop(FStateTreeExecutionContext& Context) const
+{
+	FStateTreeEvaluatorCommonBase::TreeStop(Context);
+}
+
+void FStateTreeEvaluator_GetTargetLocation::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	FStateTreeEvaluatorCommonBase::Tick(Context, DeltaTime);
+
+	FInstanceDataType* InstanceData = &Context.GetInstanceData<FInstanceDataType>(*this);
+	
+	if(!InstanceData->TargetActor)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Target Actor is null"))
+		return;
+	}
+
+	InstanceData->TargetLocation = InstanceData->TargetActor->GetActorLocation();
+
+}
---
+++ FromTheAshesReborn/Public/StateTree/Evaluators/STE_GetTargetLocation.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeEvaluatorBase.h"
+#include "STE_GetTargetLocation.generated.h"
+
+USTRUCT()
+struct FROMTHEASHESREBORN_API FStateTreeEvaluator_GetTargetLocation_InstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	TObjectPtr<AActor> TargetActor = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = Output)
+	FVector TargetLocation;
+	
+};
+
+USTRUCT(DisplayName = "Get Target Location")
+struct FROMTHEASHESREBORN_API FStateTreeEvaluator_GetTargetLocation : public FStateTreeEvaluatorCommonBase
+{
+	GENERATED_BODY()
+
+	using FInstanceDataType = FStateTreeEvaluator_GetTargetLocation_InstanceData;
+	virtual const UStruct* GetInstanceDataType() const override
+	{
+		return FInstanceDataType::StaticStruct();
+	}
+
+protected:
+	
+	virtual void TreeStart(FStateTreeExecutionContext& Context) const override;
+	virtual void TreeStop(FStateTreeExecutionContext& Context) const override;
+	virtual void Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+	
+};
