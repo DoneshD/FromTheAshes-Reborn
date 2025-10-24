8b5901af0246b484d97621862827765bea37e8df
DoneshD <donesh.dava@gmail.com>
2025-10-24 10:37:19 -0500

    Creating group combat subsystem

---
+++ FromTheAshesReborn/Private/Enemy/GroupCombatSubsystem.cpp
@@ -0,0 +1,13 @@
+﻿#include "Enemy/GroupCombatSubsystem.h"
+
+void UGroupCombatSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+}
+
+void UGroupCombatSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+	
+}
---
+++ FromTheAshesReborn/Public/Enemy/GroupCombatSubsystem.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "GroupCombatSubsystem.generated.h"
+
+UCLASS()
+class FROMTHEASHESREBORN_API UGroupCombatSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+protected:
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	
+};
