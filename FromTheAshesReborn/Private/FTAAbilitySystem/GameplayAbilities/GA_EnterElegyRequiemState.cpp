b6e19a176089509120a844640d10dab7112d049e
DoneshD <donesh.dava@gmail.com>
2026-01-01 04:42:47 -0600

    Added elegyrequiem

---
+++ FromTheAshesReborn/Private/FTAAbilitySystem/GameplayAbilities/GA_EnterElegyRequiemState.cpp
@@ -0,0 +1,38 @@
+﻿#include "FTAAbilitySystem/GameplayAbilities/GA_EnterElegyRequiemState.h"
+
+UGA_EnterElegyRequiemState::UGA_EnterElegyRequiemState()
+{
+}
+
+void UGA_EnterElegyRequiemState::OnAbilityTick(float DeltaTime)
+{
+	Super::OnAbilityTick(DeltaTime);
+}
+
+bool UGA_EnterElegyRequiemState::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, FGameplayTagContainer* OptionalRelevantTags) const
+{
+	return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
+}
+
+void UGA_EnterElegyRequiemState::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UGA_EnterElegyRequiemState::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UGA_EnterElegyRequiemState::InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	Super::InputPressed(Handle, ActorInfo, ActivationInfo);
+}
+
+void UGA_EnterElegyRequiemState::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	Super::InputReleased(Handle, ActorInfo, ActivationInfo);
+
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	
+}
---
+++ FromTheAshesReborn/Public/FTAAbilitySystem/GameplayAbilities/GA_EnterElegyRequiemState.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "FTAGameplayAbility.h"
+#include "GA_EnterElegyRequiemState.generated.h"
+
+UCLASS()
+class FROMTHEASHESREBORN_API UGA_EnterElegyRequiemState : public UFTAGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	
+	UGA_EnterElegyRequiemState();
+
+	virtual void OnAbilityTick(float DeltaTime) override;
+
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	virtual void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+
+};
