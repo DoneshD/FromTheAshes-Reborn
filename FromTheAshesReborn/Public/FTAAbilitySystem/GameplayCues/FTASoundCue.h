6ded0a892d0bec7b0b93f9e8b2e9afb6670a2dac
DoneshD <donesh.dava@gmail.com>
2026-01-23 05:04:27 -0600

    Sound cue object

---
+++ FromTheAshesReborn/Public/FTAAbilitySystem/GameplayCues/FTASoundCue.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "FTASoundCue.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSoundCueStruct
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TObjectPtr<USoundBase> SoundsArray;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float VolumeMultiplier = 1.0;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float MinPitch = 1.0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float MaxPitch = 1.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float StartTime = 0.0;
+	
+};
+
+UCLASS()
+class FROMTHEASHESREBORN_API UFTASoundCue : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<FSoundCueStruct> SoundCueArray;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float DefaultVolumeMultiplier = 1.0;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float DefaultMinPitch = 1.0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float DefaultMaxPitch = 1.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float DefaultStartTime = 0.0;
+};
