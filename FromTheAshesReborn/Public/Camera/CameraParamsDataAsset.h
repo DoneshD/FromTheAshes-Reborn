4239074f5cd628272d9527d8f6c121746dfa4ce6
DoneshD <donesh.dava@gmail.com>
2026-02-15 23:47:20 -0600

    Building DA

--- FromTheAshesReborn/Public/Camera/CameraParamsDataAsset.h
+++ FromTheAshesReborn/Public/Camera/CameraParamsDataAsset.h
@@ -5,6 +5,107 @@
 #include "CameraParamsDataAsset.generated.h"
 
 USTRUCT(BlueprintType)
+struct FSpringArmParams
+{
+	GENERATED_BODY()
+
+	FSpringArmParams()
+	: ShouldAdjustArmLength(true)
+	, ShouldOverrideArmLength(false)
+	, ShouldResetOffset(false)
+	, DeltaArmLength(0.0f)
+	, DeltaArmLengthInterpSpeed(5.0f)
+	{}
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Arm Length")
+	bool ShouldAdjustArmLength = true;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Arm Length", meta=(EditCondition="ShouldAdjustArmLength"))
+	bool ShouldOverrideArmLength = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Spring Arm")
+	float TargetArmLength = 250.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Arm Length", meta=(EditCondition="ShouldAdjustArmLength"))
+	bool ShouldResetOffset = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Arm Length", meta=(EditCondition="ShouldAdjustArmLength"))
+	float DeltaArmLength = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Arm Length", meta=(EditCondition="ShouldAdjustArmLength"))
+	float DeltaArmLengthInterpSpeed = 5.0f;
+	
+};
+
+USTRUCT(BlueprintType)
+struct FTestCameraComponentParams
+{
+	GENERATED_BODY()
+
+	FTestCameraComponentParams()
+	: ShouldAdjustFOV(false)
+	, ShouldOverrideFOV(false)
+	, ShouldResetFOVOffset(false)
+	, DeltaFOV(0.0f)
+	, DeltaFOVInterpSpeed(5.0f)
+	{}
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | FOV")
+	bool ShouldAdjustFOV = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | FOV", meta=(EditCondition="ShouldAdjustFOV"))
+	bool ShouldOverrideFOV = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | FOV", meta=(EditCondition="ShouldAdjustFOV"))
+	bool ShouldResetFOVOffset = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | FOV", meta=(EditCondition="ShouldAdjustFOV"))
+	float DeltaFOV = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | FOV", meta=(EditCondition="ShouldAdjustFOV"))
+	float DeltaFOVInterpSpeed = 5.0f;
+	
+};
+
+USTRUCT(BlueprintType)
+struct FTestCameraAnchorParams
+{
+	GENERATED_BODY()
+
+	FTestCameraAnchorParams()
+		: ShouldAdjustAnchor(false)
+		, ShouldOverrideAnchor(false)
+		, ShouldResetAnchorOffset(false)
+		, ShouldUseWorldTransform(false)
+		, NewAnchorLocation(FVector::ZeroVector)
+		, NewAnchorRotation(FRotator::ZeroRotator)
+		, DeltaAnchorInterpSpeed(5.0f)
+	{}
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera| FOV")
+	bool ShouldAdjustAnchor = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Anchor", meta=(EditCondition="ShouldAdjustAnchor"))
+	bool ShouldOverrideAnchor = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Anchor", meta=(EditCondition="ShouldAdjustAnchor"))
+	bool ShouldResetAnchorOffset = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Anchor", meta=(EditCondition="ShouldAdjustAnchor"))
+	bool ShouldUseWorldTransform = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Anchor", meta=(EditCondition="ShouldAdjustAnchor"))
+	FVector NewAnchorLocation;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Anchor", meta=(EditCondition="ShouldAdjustAnchor"))
+	FRotator NewAnchorRotation;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Camera | Anchor", meta=(EditCondition="ShouldAdjustAnchor"))
+	float DeltaAnchorInterpSpeed = 5.0f;
+	
+};
+
+USTRUCT(BlueprintType)
 struct FInputOffsetStruct
 {
 	GENERATED_BODY()
@@ -37,6 +138,15 @@ public:
 
 	UPROPERTY(EditAnywhere, Category = "Input Offset")
 	FInputOffsetStruct InputOffsetInfo;
+
+	UPROPERTY(EditAnywhere, Category = "Input Offset")
+	FSpringArmParams SpringArmParams;
+
+	UPROPERTY(EditAnywhere, Category = "Input Offset")
+	FTestCameraComponentParams CameraComponentParams;
+
+	UPROPERTY(EditAnywhere, Category = "Input Offset")
+	FTestCameraAnchorParams CameraAnchorParams;
 
 	UPROPERTY(EditAnywhere, Category = "Catch up")
 	float CatchupInterpSpeed = 8.0f;
