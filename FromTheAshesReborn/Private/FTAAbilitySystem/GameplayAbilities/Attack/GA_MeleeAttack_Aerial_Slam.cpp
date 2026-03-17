1cd95f8793f893ad04c7985c7726054616af3881
DoneshD <donesh.dava@gmail.com>
2026-03-16 23:55:07 -0500

    Adding unique attack

---
+++ FromTheAshesReborn/Private/FTAAbilitySystem/GameplayAbilities/Attack/GA_MeleeAttack_Aerial_Slam.cpp
@@ -0,0 +1,4 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FTAAbilitySystem/GameplayAbilities/Attack/GA_MeleeAttack_Aerial_Slam.h"
---
+++ FromTheAshesReborn/Public/FTAAbilitySystem/GameplayAbilities/Attack/GA_MeleeAttack_Aerial_Slam.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GA_MeleeAttack.h"
+#include "GA_MeleeAttack_Aerial_Slam.generated.h"
+
+UCLASS()
+class FROMTHEASHESREBORN_API UGA_MeleeAttack_Aerial_Slam : public UGA_MeleeAttack
+{
+	GENERATED_BODY()
+};
